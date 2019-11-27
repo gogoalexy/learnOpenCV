ea8163fdb064e7700629eb39152e21f81d99bc28
Alex Yao <cktfg18nthu@gapp.nthu.edu.tw>
2019-11-27 11:19:09 +0800

    Farneback from the scratch test suite.

---
+++ testFarneback.py
@@ -0,0 +1,14 @@
+from numpy.testing import assert_array_equal
+import cv2
+
+#import farneback
+
+path = "assets/Wooden/"
+
+prvs = cv2.imread(path + "frame10.png", cv2.IMREAD_GRAYSCALE)
+curr = cv2.imread(path + "frame11.png", cv2.IMREAD_GRAYSCALE)
+
+flow = cv2.calcOpticalFlowFarneback(prvs, curr, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+scratchFlow = []
+
+assert_array_equal(flow, scratchFlow)
